38ccf285ab65d07694aced4d67139cf57d15be4f
ikjo39 <chi4321@naver.com>
2023-12-06 15:15:05 +0900

    feat: MVC 패턴 기반 적용 - view 로직 중 화면 입력을 담당할 InputView 추가 - view 로직 중 화면 출력을 담당할 OutputView 추가 - controller 추가

---
+++ src/main/java/pairmatching/controller/PairMatchingController.java
@@ -0,0 +1,18 @@
+package pairmatching.controller;
+
+import pairmatching.view.InputView;
+import pairmatching.view.OutputView;
+
+public class PairMatchingController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public PairMatchingController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+}
